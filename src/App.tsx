225ebe1931eb1495c6441a3bf808a9f1deff5634
fmm312 <felipe@somacontadigital.com.br>
2021-05-08 22:33:05 -0300

    Configurações iniciais

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import light from './styles/themes/light';
+import dark from './styles/themes/dark';
+import GlobalStyles from './styles/global';
+
+import Routes from './routes';
 
 function App() {
-  return <div className="App">asdasd</div>;
+  return (
+    <ThemeProvider theme={dark}>
+      <GlobalStyles />
+      <Routes />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/store/actions/actionExample.ts
@@ -0,0 +1,6 @@
+export const toggleLesson = myState => {
+  return {
+    type: 'SET_MY_STATE',
+    myState,
+  };
+};
